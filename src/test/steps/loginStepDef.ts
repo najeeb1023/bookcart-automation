55a2b537bffc72fd14d86595a2afdd979e74b96e
najeeb1023 <najeeb.capricorn@gmail.com>
2023-10-31 23:02:13 +0100

    Initial commit, fixed.

--- src/test/steps/loginStepDef.ts
+++ src/test/steps/loginStepDef.ts
@@ -1,5 +1,45 @@
 import { Given, Then, When, setDefaultTimeout } from "@cucumber/cucumber"
+import { chromium, Page, Browser } from "@playwright/test"
 
-Given('User navigates to the application', async () => {
+let browser: Browser;
+let page: Page;
+
+  Given('User navigates to the application', async function () {
+    browser = await chromium.launch({headless: false, slowMo: 300});
+    page = await browser.newPage();
+    await page.goto('https://bookcart.azurewebsites.net/');
+  });
+
+
+  Given('User clicks on the login link', async function () {
+    await page.locator("//span[text()='Login']").click();
     
-})
+  });
+
+
+  Given('User enter the username as {string}', async function (username:string) {
+    await page.locator("input[formcontrolname='username']").type(username);
+  });
+
+
+  Given('User enter the password as {string}', async function (password: string) {
+    await page.locator("input[formcontrolname='password']").type(password);
+  });
+
+
+
+  When('User click on the login button', async function () {
+    await page.locator("button[color='primary']").click();
+  });
+
+
+  Then('Login should be success', async function () {
+    const text = await page.locator("//button[contains(@class, 'mat-focus-indicator mat-menu-trigger')]//span[1]").textContent() // to look into
+    console.log("Username: "+text);
+  });
+
+  When('Login should fail', async function () {
+    const failureMessage = page.locator("mat-error[role='alert']");
+
+  });
+
