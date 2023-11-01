f7ae6901ee028e378e47ff68bb131b3c71b330a5
najeeb1023 <najeeb.capricorn@gmail.com>
2023-11-01 23:09:43 +0100

    Interface for locators added.

--- src/pages/addFictionBook.ts
+++ src/pages/addFictionBook.ts
@@ -1,10 +1,16 @@
 import{ Page, expect } from "@playwright/test";
+import * as addFictionBook from "../test/resources/addFictionBook.json";
 import{ pageFixture } from "../hooks/pageFixture";
+import { PageElement } from "../test/resources/interfaces/iPageElement";
+
+    function getResource(resourceName: string){
+        return addFictionBook.webElements.find((element: PageElement) => element.elementName == resourceName) as PageElement;
+    }
 
 export class AddFiction{
     fictionBookLocators ={
-    bookCategoryTable: () => pageFixture.page.locator("//div[@class='filter-container']"),
-    selectedbook:() => pageFixture.page.locator("//div[@class='card-title']"),
+    bookCategoryTable: () => pageFixture.page.locator(getResource("bookCategoryTable").selectorValue),
+    selectedbook:() => pageFixture.page.locator(getResource("selectedBook").selectorValue),
 } 
 
 constructor(public page:Page){
